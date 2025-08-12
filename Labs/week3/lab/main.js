e219446a7e1f1d2d2b2b38987b52d71d6d8d0364
Ben Leahy <benleahy41@gmail.com>
2025-08-12 10:51:50 +1000

    week 3 lab task 1

---
+++ Labs/week3/lab/main.js
@@ -0,0 +1,43 @@
+// TODO:
+// What is the point in the directory path.join
+const express = require("express");
+const path = require("path");
+
+const print = console.log;
+const VIEWS_PATH = path.join(__dirname, "/views/"); //Important
+
+const PORT_NUMBER = 8080;
+
+let app = express();
+app.listen(PORT_NUMBER, function () {
+  print(`listening on port ${PORT_NUMBER}`);
+});
+
+app.get("/", function (req, res) {
+  fileName = VIEWS_PATH + "index.html";
+  res.sendFile(fileName);
+});
+
+app.get("/info", function (req, res) {
+  fileName = VIEWS_PATH + "info.html";
+  res.sendFile(fileName);
+});
+
+app.get("/add/:no1/:no2", function (req, res) {
+  // parameter
+  fileName = VIEWS_PATH + "index.html";
+  let number1 = parseInt(req.params.no1);
+  let number2 = parseInt(req.params.no2);
+  let result = number1 + number2;
+  res.send(String(result)); //does not accept number
+});
+
+app.get("/sub", function (req, res) {
+  // query string
+  let number1 = parseInt(req.query.no1);
+  let number2 = parseInt(req.query.no2);
+  let result = number1 - number2;
+  res.send(result + ""); // a different way to convert to strings
+});
+
+console.log("http://localhost:8080/");
