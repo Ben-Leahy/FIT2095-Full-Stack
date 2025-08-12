4296644f069e9d0fc3a6e740f3ffff4aaa79f428
Ben Leahy <benleahy41@gmail.com>
2025-08-12 11:06:24 +1000

    Week 3 lab 3 part 1 finished

--- Labs/week3/lab/main.js
+++ Labs/week3/lab/main.js
@@ -9,6 +9,7 @@ const VIEWS_PATH = path.join(__dirname, "/views/"); //Important
 const PORT_NUMBER = 8080;
 
 let app = express();
+app.use(express.static("node_modules/bootstrap/dist/css"));
 app.listen(PORT_NUMBER, function () {
   print(`listening on port ${PORT_NUMBER}`);
 });
