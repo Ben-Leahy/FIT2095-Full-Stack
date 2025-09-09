fc3a1d39985c06fb6c8150d9871c294f366ddefd
Ben Leahy <benleahy41@gmail.com>
2025-09-09 11:16:08 +1000

    schemas for assessed lab

---
+++ week7/assessed_lab/models/Book.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    // Our options are to add a unique ID, or ensure that bookTitle is unique.
+    bookId: {
+        type: String,
+        required: [true, 'Book ID is required'],
+        unique: true,
+        trim: true,
+        uppercase: true
+    },
+    bookTitle: {
+        type: String,
+        required: [true, 'Book title is required'],
+        trim: true,
+        minlength: [2, 'Book title must be at least 2 characters'],
+        maxlength: [50, 'Book title cannot exceed 50 characters']
+    },
+    bookAuthur: {
+        type: String,
+        required: [true, 'Book authur is required'],
+        trim: true,
+        minlength: [2, 'Book authur must be at least 2 characters'],
+        maxlength: [50, 'Book authur cannot exceed 200 characters']
+    },
+    rentalPrice: {
+        type: Number,
+        required: [true, 'Daily rental price is required'],
+        min: [0.01, 'Daily rental price must be between $0.01 and $50'],
+        max: [50, 'Daily rental price must be between $0.01 and $50']
+    },
+}, {
+    timestamps: true // Adds createdAt and updatedAt fields automatically
+});
+
+// Create indexes for better query performance
+studentSchema.index({ bookId: 1 }); // ascending
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
---
+++ week7/assessed_lab/models/Rental.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const rentalSchema = new mongoose.Schema({
+    // Our options are to add a unique ID, or ensure that bookTitle is unique.
+    rentalId: {
+        type: String,
+        required: [true, 'Rental ID is required'],
+        unique: true,
+        trim: true,
+        uppercase: true
+    },
+    customerName: {
+        type: String,
+        required: [true, 'Customer Name is required'],
+        trim: true,
+        minlength: [2, 'Customer Name must be at least 2 characters'],
+        maxlength: [50, 'Customer Name cannot exceed 50 characters']
+    },
+    rentalDays: {
+        type: Number,
+        required: [true, 'Number of rental days is required'],
+        min: [0, 'Number of rental days must be between 0 and 365'],
+        max: [365, 'Number of rental days must be between 0 and 365']
+    },
+    totalCost: {
+        type: Number,
+        required: [true, 'Total rental cost is required'],
+        min: [0.01, 'Total rental cost must be between $0.01 and $18250'], //$50 * 365 days
+        max: [18250, 'Total rental cost must be between $0.01 and $18250']
+    },
+    bookId: {
+        type: String,
+        required: [true, 'Book ID is required'],
+        unique: true,
+        trim: true,
+        uppercase: true
+    },
+}, {
+    timestamps: true // Adds createdAt and updatedAt fields automatically
+});
+
+// Create indexes for better query performance
+studentSchema.index({ bookId: 1 }); // ascending
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
